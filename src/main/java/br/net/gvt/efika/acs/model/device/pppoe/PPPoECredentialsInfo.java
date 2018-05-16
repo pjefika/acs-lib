5b948566738540aa99a776f2b5b5ab577ffe5b58
Agibert <matheusagibert@gmail.com>
2018-05-16 12:24:57 -0300

    refactored imports

---
+++ src/main/java/br/net/gvt/efika/acs/model/device/pppoe/PPPoECredentialsInfo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.net.gvt.efika.acs.model.device.pppoe;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import br.net.gvt.efika.acs.parser.device.PPPoECredentialsInfoDeserializer;
+import br.net.gvt.efika.acs.parser.device.PPPoECredentialsInfoSerializer;
+
+/**
+ *
+ * @author G0042204
+ */
+@JsonDeserialize(using = PPPoECredentialsInfoDeserializer.class)
+@JsonSerialize(using = PPPoECredentialsInfoSerializer.class)
+public class PPPoECredentialsInfo {
+
+    private String Username, Password;
+
+    public PPPoECredentialsInfo() {
+    }
+
+    public PPPoECredentialsInfo(String username, String password) {
+        this.Username = username;
+        this.Password = password;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        this.Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        this.Password = password;
+    }
+
+}
