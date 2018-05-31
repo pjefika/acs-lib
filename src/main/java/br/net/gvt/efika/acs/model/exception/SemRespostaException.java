46c03106ebc04c4d8ff6252a66be0732aa5310b6
Agibert <matheusagibert@gmail.com>
2018-05-31 20:31:42 -0300

    adds SemRespostaException

---
+++ src/main/java/br/net/gvt/efika/acs/model/exception/SemRespostaException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.net.gvt.efika.acs.model.exception;
+
+/**
+ *
+ * @author G0042204
+ */
+public class SemRespostaException extends Exception {
+
+    public SemRespostaException() {
+        super("Não houve resposta ao comando.");
+    }
+
+    public SemRespostaException(String message) {
+        super(message);
+    }
+
+}
